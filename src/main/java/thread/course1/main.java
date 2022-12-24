e6d7eedfaa509f78173daeab7b481b4612803396
Arvind Bhatt <arvindbhatt066@gmail.com>
2022-12-25 01:34:36 +0530

    deleted giles

--- src/main/java/thread/course1/main.java
+++ src/main/java/thread/course1/main.java
@@ -1,11 +1,8 @@
 package thread.course1;
 
-import lombok.extern.slf4j.Slf4j;
 
-import java.io.Console;
-import java.util.logging.ConsoleHandler;
+import java.lang.Thread.UncaughtExceptionHandler;
 
-@Slf4j
 public class main {
 
     public static void main(String[] args) {
@@ -13,17 +10,35 @@ public class main {
         Thread thread = new Thread(()->{
             System.out.println("Thread name :" + Thread.currentThread().getName() + "Thread Priority" +
                                Thread.currentThread().getPriority());
-            throw new RuntimeException("Intentional Exception");
+//            throw new RuntimeException("Intentional Exception");
         });
 
-        thread.setPriority(Thread.MAX_PRIORITY);
-        try {
+        // thread.setPriority(Thread.MAX_PRIORITY);
+        /*thread.setUncaughtExceptionHandler(new UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println(t.getName() + " Uncaught Exception Occured");
+            }
+        });*/
+
+        thread.start();
+        /*try {
             thread.start();
-        } catch (Exception e){
-            log.info("Exception :: {}", e);
+        } catch (RuntimeException e){
             System.out.println("Exception occured");
-        }
-        System.out.println("Main Thread " + Thread.currentThread().getName() + "Thread Priority : " +
+        }*/
+        /*System.out.println("Main Thread " + Thread.currentThread().getName() + "Thread Priority : " +
                            Thread.currentThread().getPriority());
+
+        NewThread newThread = new NewThread();
+        newThread.start();*/
+    }
+
+    public static class NewThread extends Thread {
+
+        @Override
+        public void run() {
+            System.out.println("This Thead is class based " + Thread.currentThread().getName());
+        }
     }
 }
